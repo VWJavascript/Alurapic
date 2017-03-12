0552bfd1ed7054c96257540044441e2da1c39055
Victor Arnaud <victorhad@gmail.com>
2017-03-12 17:40:49 -0300

    Module e Loader

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-@Component ({
+@Component({
   selector: 'app',
   templateUrl: './app/app.component.html'
 })
-class AppComponent {}
+export class AppComponent {}
