2f1dcae932f197feb885a8ec40dc0830e6cf8890
Victor Arnaud <victorhad@gmail.com>
2017-03-12 21:07:56 -0300

    API and photo component

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
+  moduleId: module.id,
   selector: 'app',
-  templateUrl: './app/app.component.html'
+  templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  photos: Object[] = [];
+  constructor(http: Http) {
+    http
+      .get('v1/fotos')
+      .map(response => response.json())
+      .subscribe(
+        photos => {
+          this.photos = photos;
+          console.log(this.photos)
+        },
+        erro => console.log(erro);
+      );
+  }
+}
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
+import {PhotoModule} from './photo/photo.module';
+import 'rxjs/add/operator/map';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, PhotoModule, HttpModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
